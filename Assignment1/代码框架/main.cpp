746d7c0ccfc1ebe7f9df1f25c05132c378adb32b
Zijun Yu <yu1319324245@gmail.com>
2021-03-27 21:42:56 +0800

    done: assignment 1

--- Assignment1/代码框架/main.cpp
+++ Assignment1/代码框架/main.cpp
@@ -19,27 +19,80 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     return view;
 }
 
+Eigen::Matrix4f get_rotation(Vector3f axis, float angle)
+{
+    Eigen::Matrix4f rotation = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix3f identity = Eigen::Matrix3f::Identity();
+    Eigen::Matrix3f rotation2 = Eigen::Matrix3f::Identity();
+    Eigen::Matrix3f dual = Eigen::Matrix3f::Identity();
+    float angleRadian = angle / 180 * MY_PI;
+
+    dual << 0, -axis.z(), axis.y(),
+            axis.z(), 0, -axis.x(),
+            -axis.y(), axis.x(), 0;
+
+    rotation2 = std::cos(angleRadian) * identity
+               + (1 - std::cos(angleRadian)) * axis * axis.transpose()
+               + std::sin(angleRadian) * dual;
+    
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            rotation(i, j) = rotation2(i, j);
+
+    return rotation;
+}
+
 Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
-    // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
+    float angle = rotation_angle / 180 * MY_PI;
+    model << std::cos(angle), -std::sin(angle), 0, 0,
+             std::sin(angle), std::cos(angle), 0, 0,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
 
     return model;
+
+    // Another solution.
+    Eigen::Vector3f z(0, 0, 1);
+    return get_rotation(z, rotation_angle);
 }
 
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float zNear, float zFar)
 {
-    // Students will implement this function
-
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
 
-    // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+
+    Eigen::Matrix4f perspToOrtho = Eigen::Matrix4f::Identity();
+    perspToOrtho << zNear, 0, 0, 0,
+                    0, zNear, 0, 0,
+                    0, 0, zNear + zFar, -(zNear * zFar),
+                    0, 0, 1, 0;
+
+    float height = std::tan(eye_fov / 180 * MY_PI / 2) * std::abs(zNear) * 2;
+    float width = aspect_ratio * height;
+
+    Eigen::Matrix4f ortho = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f orthoTranslate = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f orthoScale = Eigen::Matrix4f::Identity();
+    orthoTranslate << 1, 0, 0, 0,
+                      0, 1, 0, 0,
+                      0, 0, 1, -((zNear + zFar) / 2),
+                      0, 0, 0, 1;
+    orthoScale << 2 / width, 0, 0, 0,
+                  0, 2 / height, 0, 0,
+                  0, 0, 2 / (zNear - zFar), 0,
+                  0, 0, 0, 1;
+    ortho = orthoScale * orthoTranslate;
+
+    projection = ortho * perspToOrtho;
 
     return projection;
 }
